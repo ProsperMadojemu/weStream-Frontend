160c534be178b62d96f4db4d36e5e0bd81fa75a6
User-Err <prospermadojmeu00@gmail.com>
2024-11-06 12:03:12 +0100

    Project names App Fix2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home' 
 import PastStreams from './pages/PastStreams/PastStreams'
-import SignUp from './pages/SignUp/SignUp'
+import Signup from './pages/SignUp/Signup'
 import Streams from './pages/Streams/Streams'
 import Login from './pages/Login/Login'
 import About from './pages/About/About'
@@ -17,7 +17,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/past-streams' element={<PastStreams />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signup' element={<Signup />} />
         <Route path='/streams' element={<Streams />} />
         <Route path='/about' element={<About />} />
         <Route path='/login' element={<Login />} />
